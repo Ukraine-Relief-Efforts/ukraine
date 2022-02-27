1a49f20c027c6aff207061bea5a6cfd7d683ba4c
Javier Gonzalez <javier.e.glz@gmail.com>
2022-02-26 22:14:01 -0600

    linting

--- components/OrgCard.js
+++ components/OrgCard.js
@@ -2,14 +2,14 @@ function OrgCard(props) {
   return (
     <div className="m-5 p-5 shadow-lg rounded-lg border border-gray-100">
       {props.values.map((item, index) => {
-		return (
-			<div key={index} className="col-md-4">
-                <div className="mb-4">
-                    <span>{props.titles[index]}:</span> {item}
-                </div>
-			</div>
-		    );
-		})}
+        return (
+          <div key={index} className="col-md-4">
+            <div className="mb-4">
+              <span>{props.titles[index]}:</span> {item}
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
