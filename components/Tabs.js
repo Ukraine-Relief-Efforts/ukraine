183f4045600f061eb288178a786cbafe4263c517
Birdman <geositta@protonmail.com>
2022-02-27 02:38:46 -0600

    Add tabs component

---
+++ components/Tabs.js
@@ -0,0 +1,39 @@
+const Tabs = ({ openTab, cause }) => {
+    const tabs = ["Military", "Food", "Humanitarian"];
+    return (
+        <div className="flex flex-wrap">
+            <div className="w-full">
+                <ul
+                className="flex mb-0 list-none flex-wrap pt-3 pb-4 flex-row"
+                role="tablist"
+                >
+                {tabs.map(tab => {
+                    <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
+                        <a
+                        className={
+                            "text-xs font-bold uppercase px-5 py-3 shadow-lg rounded " +
+                            "block leading-normal " +
+                            (openTab === cause
+                            ? "text-white bg-blue-600"
+                            : "text-blue-600 bg-white")
+                        }
+                        onClick={e => {
+                            e.preventDefault();
+                            this.props.onClick(cause);
+                        }}
+                        data-toggle="tab"
+                        href="#link1"
+                        role="tablist"
+                        >
+                        {tab}
+                        </a>
+                    </li>
+                })}
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Tabs;
+
