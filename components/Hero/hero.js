7096ec811f90011575ed014e1af45a9c0bf6d5e6
Eugene Chan <me@eugenechantk.me>
2022-02-28 05:11:25 +0000

    Fixed responsive styling for nav bar

--- components/Hero/hero.js
+++ components/Hero/hero.js
@@ -4,8 +4,7 @@ export default function Hero () {
     return (
         <div className='mt-24 mb-14'>
             <div>
-                <h1 className={`${styles.h1} font-extrabold text-5xl md:text-7xl lg:text-8xl inline leading-none
-                sm:text-9xl`}>Help </h1>
+                <h1 className={`${styles.h1} font-extrabold text-5xl md:text-7xl lg:text-8xl inline leading-none`}>Help </h1>
                 <span className={`${styles.ukraine} font-extrabold px-2 py-0.25 text-5xl md:text-7xl lg:text-8xl rounded-lg md:rounded-xl lg:rounded-2xl leading-none`}>
                     Ukraine
                 </span>
--- components/Nav/nav.js
+++ components/Nav/nav.js
@@ -5,18 +5,24 @@ import Link from "next/link"
 export default function NavBar () {
     return (
         <div>
-            <div className="flex flex-row items-center gap-6 font-bold text-base">
-                <div className="w-10">
-                    <Image 
-                        src={logo}
-                        alt='Ukraine Donations'
-                        layout="intrinsic"
-                    />
-                </div> 
-                <Link href="about">
-                    <a className="leading-tight">About us</a>
-                </Link>
-                <a href="#" className="leading-tight">Apply for funding</a>
+            <div className="flex flex-row items-center gap-6 md:gap-10 lg:gap-16">
+                <div className="w-10 sm:w-14 lg:w-16">
+                    <Link href="/">
+                        <a>
+                            <Image 
+                                src={logo}
+                                alt='Ukraine Donations'
+                                layout="intrinsic"
+                            />
+                        </a>
+                    </Link>
+                </div>
+                <div className="flex flex-row items-center gap-4 lg:gap-10 font-bold text-base md:text-xl lg:text-2xl"> 
+                    <Link href="about">
+                        <a className="leading-tight">About us</a>
+                    </Link>
+                    <a href="#" className="leading-tight">Apply for funding</a>
+                </div>
             </div>
             
         </div>
