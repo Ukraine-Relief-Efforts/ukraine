c5c720b327edfc52d6d0aedb76dfda9e79c33e21
Eugene Chan <me@eugenechantk.me>
2022-02-28 22:00:20 +0000

    fixed Open Sans display issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
@@ -6,7 +8,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'open': ['"Open Sans"', 'Georgia', 'Arial'],
+        sans: ['Open Sans',...defaultTheme.fontFamily.sans],
      },
     },
   },
