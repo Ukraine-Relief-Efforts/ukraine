1aa6dca2bb50ea03d5755ae30341381007168dd1
Eugene Chan <me@eugenechantk.me>
2022-03-05 04:06:57 +0000

    Update FAQ format

--- components/SubPage/FAQ/OneFaq.js
+++ components/SubPage/FAQ/OneFaq.js
@@ -2,12 +2,7 @@ export default function OneFaq (props) {
     return (
         <div>
             <div className="py-8">
-                <h4 className=" text-2xl font-bold text-uablue-default mb-4">{props.title}</h4>
-                {props.tip && 
-                <div className="flex items-center mb-4">
-                    <span className="mr-2">👉 </span>
-                    <span>{props.tip}</span>
-                </div>}
+                <h4 className=" text-2xl font-bold text-uablue-default mb-4 tracking-tight">{props.title}</h4>
                 {props.children}
             </div>
             <hr></hr>
