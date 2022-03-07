31960af406b8769df2e63f491acabcbd6211905e
Birdman <geositta@protonmail.com>
2022-03-07 10:28:48 -0600

    Add correct content on For Donors page

--- pages/for-donors.js
+++ pages/for-donors.js
@@ -6,34 +6,19 @@ import Link from 'next/link';
 export default function ForDonors() {
   return (
     <Layout>
-      <div className="w-full md:w-3/4 lg:w-2/3 pb-16">
+      <div className="w-full md:w-3/4 lg:w-2/3 pb-8">
         <Hero
           title="For donors"
-          description="We’ve compiled some helpful information to give you a better understanding of how to pick the right project to support, and how to transfer funds."
+          description="Helping you vet and support fundraising for Ukraine."
         />
-        <div className="mt-24">
-          <h1 className="font-extrabold text-2xl lg:text-4xl">
-            About this initiative
-          </h1>
-          <div className="mt-8">
-            <p>
-              We have created this website to help Ukrainian fundraisers reach
-              international supporters. problem statement:
-            </p>
-            <br />
-            <p>
-              One of our main goals was to make evaluation of smaller
-              volunteer-driven fundraisers simple.Much of the work on
-              coordinating emergency humanitarian aid to Ukrainians is done by
-              volunteers.These are friends and family members of people who need
-              help, political activists, and self-organized groups within the
-              Ukrainian diaspora worldwide.They are in direct contact with
-              people on the ground and understand their immediate needs
-              best.They know what’s needed and where and help coordinate
-              fundraising, purchase, and delivery of medical supplies, safety
-              equipment, food etc.
-            </p>
-          </div>
+        <div className="mt-8">
+        <p>
+            We created this website to help the international community donate
+            to Ukrainians in need. In addition to hosting the key information
+            about major donation avenues, we have also sought out volunteer
+            groups coordinating purchases of emergency supplies for both
+            civilians and military in Ukraine.
+        </p>
         </div>
         <div className="mt-24">
           <h1 className="font-extrabold text-2xl lg:text-4xl mb-2">F.A.Q.</h1>
