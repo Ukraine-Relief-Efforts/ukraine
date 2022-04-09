7e7dfc684e840141adb5a22969cd2bf7db5b1e88
Bradley Reynolds <bradley.reynolds@darbia.dev>
2022-04-09 01:27:30 -0500

    Remove GA

--- pages/_app.js
+++ pages/_app.js
@@ -2,24 +2,11 @@ import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import { appWithTranslation } from 'next-i18next';
 
-import * as ga from '../lib/ga'
 import '../styles/globals.css'
 
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
-
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      ga.pageview(url)
-    }
-
-    router.events.on('routeChangeComplete', handleRouteChange)
-
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
 
   return <Component {...pageProps} />
 }
