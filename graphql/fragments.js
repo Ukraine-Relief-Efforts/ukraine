b5bc80128ccac39ba0d97639666a1c717fb2cf2b
Birdman <geositta@protonmail.com>
2022-04-19 16:33:13 -0500

    Add graphql contentful query

---
+++ graphql/fragments.js
@@ -0,0 +1,7 @@
+export const FRAGMENT_CONTENTFUL_FUNDRAISER = `
+    fragment FundraiserFields on Fundraisers {
+        organizationName
+        donationLink
+        cause
+    }
+`;
---
+++ graphql/queries.js
@@ -0,0 +1,10 @@
+export const fundraiserQuery = `
+    query {
+        fundraiser(id:"5zy3mVAEbNg9K7XXNF0Z4a") {
+            organizationName
+            donationLink
+            cause
+        }
+    }
+`;
+
